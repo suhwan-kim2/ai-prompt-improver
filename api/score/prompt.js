efad77f2a4ffbe82356bfafbf66ffc30656225bc
suhwan-kim2 <saykimsu@gmail.com>
2025-08-26 21:55:21 +0900

    Create prompt.js

---
+++ api/score/prompt.js
@@ -0,0 +1,13 @@
+
+//prompt.js
+import { EvaluationSystem } from "../../utils/evaluationSystem.js";
+import { readJson } from "../_helpers.js";
+
+const evaluator = new EvaluationSystem();
+
+export default async function handler(req, res) {
+  if (req.method !== "POST") return res.status(405).end();
+  const { prompt = "", domain = "development" } = await readJson(req);
+  const out = evaluator.evaluatePromptQuality(prompt, domain);
+  res.status(200).json(out);
+}
